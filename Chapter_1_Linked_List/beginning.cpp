a969fadd3da10a4a9e3c3f61f6bc8fbdeaf16ac0
Pratham <mishrapratham.codeit@gmail.com>
2023-08-09 23:46:36 +0530

    chapter_1: inserting the node at the beginning of the linked list

---
+++ Chapter_1_Linked_List/beginning.cpp
@@ -0,0 +1,46 @@
+// Inserting a node at the beginning of the linked list.
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+struct Node *head;
+void Insert(int x)
+{
+    Node *temp = new Node();
+    temp->data = x;
+    temp->next = NULL;
+    if (head != NULL)
+        temp->next = head;
+    head = temp;
+}
+void Print()
+{
+    Node *temp = head;
+    cout << "List is: ";
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    head = NULL;
+    cout << "How many numbers?" << endl;
+    int n, i, x;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter the number" << endl;
+        cin >> x;
+        Insert(x);
+        Print();
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Data Structures in C++.
 ### Folder Structure
 
 1. `Chapter 1: Linked List Implementation`
+   - Inserting the node at the beginning of the linked list. (beginning.cpp)
