8164ae7bbc56ff5005b049f1af021a939f4317c3
Pratham <mishrapratham.codeit@gmail.com>
2023-09-22 02:06:03 +0530

    up

--- pointers/ii_working.cpp
+++ pointers/ii_working.cpp
@@ -9,9 +9,9 @@ int main()
     int *p;
     p = &a;
 
-    cout << int(p) << endl;
+    cout << (int)p << endl;
     cout << "Size of integer is " << sizeof(int) << endl;
-    cout << "Arithmetic addition of a pointer: " << int(p + 2) << endl; // Addding 1 to the pointer doesn't mean it will increase the address by 1. Here, the pointer datatype is (int) so if p at first contain address value of 2003, it will after the arithmetic addition will contain value 2007 i.e., because of the size (int) datatype takes.
+    cout << "Arithmetic addition of a pointer: " << (int)(p + 2) << endl; // Addding 1 to the pointer doesn't mean it will increase the address by 1. Here, the pointer datatype is (int) so if p at first contain address value of 2003, it will after the arithmetic addition will contain value 2007 i.e., because of the size (int) datatype takes.
     return 0;
 }
 
