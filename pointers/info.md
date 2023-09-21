b6e84476add5788073c94b4689d744768cf6d4fb
Pratham <mishrapratham.codeit@gmail.com>
2023-09-22 01:00:50 +0530

    added intro and info

---
+++ pointers/info.md
@@ -0,0 +1,15 @@
+## Pointers
+
+Variables that store address of another variable.
+
+A simple implementation:
+
+```c++
+int a; // A normal declaration.
+int *p; // Declaring a pointer variable.
+p = &a; // Storing the address of a in p.
+
+// Here, if we try to print *p. It will print the value of the address it is pointed to.
+```
+
+> Check out /pointers/intro.cpp for the code snippets.
---
+++ pointers/intro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int *p;
+    p = &a;
+
+    a = 5;
+
+    cout << p << endl;  // This is the address of the variable it is pointing to.
+    cout << &a << endl; // This is the address of a.
+    cout << &p << endl; // This is the address of p.
+    cout << *p << endl; // This is the value of a i.e., the value of the address it (p) is pointing to.
+    *p = 8;             // We can change the value of the address it (p) is pointing to.
+    cout << a << endl;  // We can see the updated value here.
+}
