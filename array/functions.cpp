f0da15cf186310b9ed0235b283ab70de2cc21965
Pratham <mishrapratham.codeit@gmail.com>
2023-10-01 18:37:08 +0530

    added boiler plate code for array

---
+++ array/functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// #include "functions.h"
+
+using namespace std;
+
+void OneDimensionalArray()
+{
+}
+
+void TwoDimensionalArray()
+{
+}
+
+// FOR TESTING
+
+int main()
+{
+    return 0;
+}
---
+++ array/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <functions.h>
+
+using namespace std;
+
+int main()
+{
+    OneDimensionalArray();
+    TwoDimensionalArray();
+    return 0;
+}
