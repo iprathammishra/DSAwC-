bbfe59f8f26e76be46d08e1a467f35ae5c61fb86
Pratham <mishrapratham.codeit@gmail.com>
2024-12-29 18:13:45 +0530

    queue.cpp

---
+++ implementations/easy/queue.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T> void dbg(const T& x) { cerr << x; }
+template <typename T, typename U> void dbg(const pair<T, U>& p) { cerr << "(" << p.first << ", " << p.second << ")"; }
+template <typename T> void dbg(const vector<T>& v) { cerr << "["; for (size_t i = 0; i < v.size(); ++i) dbg(v[i]), cerr << (i + 1 < v.size() ? ", " : ""); cerr << "]"; }
+template <typename T> void dbg(const vector<vector<T>>& v) { cerr << "["; for (size_t i = 0; i < v.size(); ++i) dbg(v[i]), cerr << (i + 1 < v.size() ? ", " : ""); cerr << "]"; }
+template <typename T> void dbg(const set<T>& s) { cerr << "{"; for (auto it = s.begin(); it != s.end(); ++it) dbg(*it), cerr << (next(it) != s.end() ? ", " : ""); cerr << "}"; }
+template <typename K, typename V> void dbg(const map<K, V>& m) { cerr << "{"; for (auto it = m.begin(); it != m.end(); ++it) dbg(*it), cerr << (next(it) != m.end() ? ", " : ""); cerr << "}"; }
+template <typename T, typename... Args> void dbg(const T& arg, const Args&... args) { dbg(arg); if (sizeof...(args)) cerr << " | ", dbg(args...); }
+#define TRACE(args...) { cerr << "Line " << __LINE__ << ": "; dbg(args); cerr << endl; }
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int t; cin >> t;
+  while (t--) {
+    int n; cin >> n;
+    vector<int> a(n); for (int i = 0; i < n; i++) { cin >> a[i]; }
+    queue<int> q; for (int i = 0; i < n; i++) { q.push(a[i]); }
+    int deq; cin >> deq;
+    while (deq--) { q.pop(); }
+    cin >> n;
+    vector<int> b(n); for (int i = 0; i < n; i++) { cin >> b[i]; }
+    for (int i = 0; i < n; i++) { q.push(b[i]); }
+    vector<int> ans;
+    while (!q.empty()) {
+      ans.push_back(q.front()); q.pop();
+    }
+    TRACE(ans);
+  }
+  return 0;
+}
