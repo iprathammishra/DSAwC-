ee123b1a21d4a2cdba97f68196b4d01697c97e46
Pratham <mishrapratham.codeit@gmail.com>
2025-01-28 14:37:08 +0530

    r

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long cycle(int n) {
-  if (n <= 1) { return n; }
-  return n * cycle(n-1);
-}
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   int n; cin >> n;
-  cout << cycle(n) << endl; 
+  vector<long long> a(n); for (int i = 0; i < n; i++) { cin >> a[i]; }
+  int p1 = 0, p2 = n-1;
+  bool ok = true;
+  while (p1 < p2) {
+    if (a[p1] != a[p2]) { ok = false; break; }
+    p1++; p2--;
+  }
+  if (ok == false) { cout << "NO" << endl; }
+  else { cout << "YES" << endl; }
   return 0;
 }
