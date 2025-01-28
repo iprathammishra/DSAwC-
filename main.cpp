4fbe1dfe694b8449a13132edebb26f37364c81cb
Pratham <mishrapratham.codeit@gmail.com>
2025-01-28 14:33:09 +0530

    j

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-void cycle(int n) {
-  if (n == 1) { return; }
-  cycle(n-1);
-  cout << n-1 << endl;
+long long cycle(int n) {
+  if (n <= 1) { return n; }
+  return n * cycle(n-1);
 }
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   int n; cin >> n;
-  cycle(n+1);
+  cout << cycle(n) << endl; 
   return 0;
 }
