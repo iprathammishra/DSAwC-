8926db53520e42c6c8fdca6f765a316c4e40b662
Pratham <mishrapratham.codeit@gmail.com>
2025-01-28 14:28:32 +0530

    b

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 void cycle(int n) {
-  if (n == 0) { return; }
-  cout << "I love Recursion" << endl;
+  if (n == 1) { return; }
   cycle(n-1);
+  cout << n-1 << endl;
 }
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   int n; cin >> n;
-  cycle(n);
+  cycle(n+1);
   return 0;
 }
