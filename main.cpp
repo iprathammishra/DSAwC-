ef2908f11e9ff019a9327e2b405b971ed1db0c4b
Pratham <mishrapratham.codeit@gmail.com>
2025-01-28 13:58:40 +0530

    d

--- main.cpp
+++ main.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int tt; cin >> tt;
+  while (tt--) {
+    int a, b; cin >> a >> b;
+    if (a % b == 0) { cout << 0 << endl; }
+    else if (a < b) { cout << b - a << endl; }
+    else {
+      int x = a / b;
+      cout << b*(x+1) - a << endl;
+    }
+  }
   return 0;
 }
