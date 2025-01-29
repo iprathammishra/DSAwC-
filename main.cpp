8eb71a637c313ddeda3ff68c692fe2938d6287f7
Pratham <mishrapratham.codeit@gmail.com>
2025-01-29 10:55:29 +0530

    lcm_gcd

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+int gcd(int a, int b) {
+  if (b == 0) { return a; }
+  return gcd(b, a%b);
+}
+int lcm(int a, int b) {
+  return a * b /gcd(a, b);
+}
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   int tt; cin >> tt;
   while (tt--) {
-    ll l, r; cin >> l >> r;
-    if (2*l > r) { cout << -1 << " " << -1 << endl; }
-    else { cout << l << " " << 2*l << endl; }
+    int n; cin >> n;
+    vector<int> a(n); for (int i = 0; i < n; i++) { cin >> a[i]; }
+    int g = a[0];
+    for (int i = 1; i < n; i++) { g = gcd(g, a[i]); }
+    if (g == 1) { cout << 0 << endl; }
+    else if (gcd(g, n) == 1) { cout << 1 << endl; }
+    else if (gcd(g, n-1) == 1) { cout << 2 << endl; }
+    else { cout << 3 << endl;}
   }
   return 0;
 }
