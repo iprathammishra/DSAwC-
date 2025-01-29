f9aa890c9c5dc6e273c4edcfff402bd594af14e0
Pratham <mishrapratham.codeit@gmail.com>
2025-01-29 10:28:30 +0530

    two_divisors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+typedef long long ll;
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
+  int tt; cin >> tt;
+  while (tt--) {
+    ll a, b; cin >> a >> b;
+    if (b % a == 0) { cout << b * (b / a) << endl; }
+    else { cout << b * (a / __gcd(a, b)) << endl; }
+  }
   return 0;
 }
