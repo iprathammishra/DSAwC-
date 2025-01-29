a4429700823159453e185bb41672e8318e4de58b
Pratham <mishrapratham.codeit@gmail.com>
2025-01-29 11:47:27 +0530

    gcd

--- main.cpp
+++ main.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int gcd(int a, int b) {
-  if (b == 0) { return a; }
-  return gcd(b, a%b);
-}
-int lcm(int a, int b) {
-  return a * b /gcd(a, b);
+int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a%b); }
+int lcm(int a, int b) { return a*b/gcd(a, b); }
+pair<int, int> getRatio(int a, int b) {
+  int g = gcd(a, b);
+  a = a/g;
+  b = b/g;
+  return make_pair(a, b);
 }
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   int tt; cin >> tt;
   while (tt--) {
     int n; cin >> n;
-    vector<int> a(n); for (int i = 0; i < n; i++) { cin >> a[i]; }
-    int g = a[0];
-    for (int i = 1; i < n; i++) { g = gcd(g, a[i]); }
-    if (g == 1) { cout << 0 << endl; }
-    else if (gcd(g, n) == 1) { cout << 1 << endl; }
-    else if (gcd(g, n-1) == 1) { cout << 2 << endl; }
-    else { cout << 3 << endl;}
+    string s; cin >> s;
+    map<pair<int, int>, int> mmap;
+    int dCount = 0, kCount = 0;
+    for (char& ch : s) {
+      if (ch == 'D') dCount++; 
+      else kCount++;
+      cout << ++mmap[getRatio(dCount, kCount)] << " ";
+    }
+    cout << endl; 
   }
   return 0;
 }
