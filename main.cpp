237918e3d4d9f83fb5905aa7a9e801dc99bb01f3
Pratham <mishrapratham.codeit@gmail.com>
2025-01-29 10:39:52 +0530

    d

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   int tt; cin >> tt;
   while (tt--) {
-    ll a, b; cin >> a >> b;
-    if (b % a == 0) { cout << b * (b / a) << endl; }
-    else { cout << b * (a / __gcd(a, b)) << endl; }
+    ll l, r; cin >> l >> r;
+    if (2*l > r) { cout << -1 << " " << -1 << endl; }
+    else { cout << l << " " << 2*l << endl; }
   }
   return 0;
 }
