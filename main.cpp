e1bbc2196bbe0f571309ef1735465d8831bc0823
Pratham <mishrapratham.codeit@gmail.com>
2025-01-29 14:03:57 +0530

    d

--- main.cpp
+++ main.cpp
@@ -1,27 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a%b); }
-int lcm(int a, int b) { return a*b/gcd(a, b); }
-pair<int, int> getRatio(int a, int b) {
-  int g = gcd(a, b);
-  a = a/g;
-  b = b/g;
-  return make_pair(a, b);
-}
+
 int main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
   int tt; cin >> tt;
   while (tt--) {
     int n; cin >> n;
-    string s; cin >> s;
-    map<pair<int, int>, int> mmap;
-    int dCount = 0, kCount = 0;
-    for (char& ch : s) {
-      if (ch == 'D') dCount++; 
-      else kCount++;
-      cout << ++mmap[getRatio(dCount, kCount)] << " ";
+    vector<int> cnt(n+1, 0), mx(n+1, 0);
+    vector<int> a(n); for (int i = 0; i < n; i++) { cin >> a[i]; if (a[i] <= n) cnt[a[i]]++; }
+    for (int i = 1; i <= n; i++) {
+      for (int j = i; j <= n; j += i) {
+        mx[j] += cnt[i];
+      }
     }
-    cout << endl; 
+    cout << *max_element(begin(mx), end(mx)) << endl;
+    
   }
   return 0;
 }
