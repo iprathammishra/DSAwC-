13c13ed14c3c956daaf91e37aa11471e0d71c2f8
Pratham <mishrapratham.codeit@gmail.com>
2025-01-30 23:27:54 +0530

    d

--- main.cpp
+++ main.cpp
@@ -3,15 +3,5 @@ using namespace std;
 
 int32_t main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
-  int tt; cin >> tt;
-  while (tt--) {
-    string s; cin >> s;
-    int c = 0;
-    for (char& ch : s) {
-      if (ch == '1')
-        c++;
-    }
-    cout << c << endl;
-  }
   return 0;
 }
