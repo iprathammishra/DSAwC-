4ace5572682c287149ce826501ff051d0d439d32
Pratham <mishrapratham.codeit@gmail.com>
2025-02-01 11:48:42 +0530

    d

--- main.cpp
+++ main.cpp
@@ -4,20 +4,27 @@ using namespace std;
 
 int32_t main() {
   ios_base::sync_with_stdio(false), cin.tie(nullptr);
-  ll w, h, n; cin >> w >> h >> n;
-  ll low = max(w, h);
-  ll high = low;
-  while ((high / w) * (high / h) < n) 
-    high *= 2;
-  ll ans = high;
-  while (low <= high) {
-    ll mid = low + (high-low)/2;
-    if ((mid/w) * (mid/h) >= n) {
-      ans = mid;
-      high = mid-1;
+  ll n, k; cin >> n >> k;
+  vector<ll> a(n); 
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+  deque<ll> maxD, minD;
+  ll L = 0, R = 0;
+  ll ans = 0;
+  while (R < n) {
+    while (!maxD.empty() && a[maxD.back()] <= a[R])
+      maxD.pop_back();
+    maxD.push_back(R);
+    while (!minD.empty() && a[minD.back()] >= a[R])
+      minD.pop_back();
+    minD.push_back(R);
+    while (a[maxD.front()] - a[minD.front()] > k) {
+      if (maxD.front() == L) maxD.pop_front();
+      if (minD.front() == L) minD.pop_front();
+      L++;
     }
-    else 
-      low = mid+1;
+    ans += (R - L + 1);
+    R++;
   }
   cout << ans << endl;
   return 0;
