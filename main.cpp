af2b156817540a8fba7e4a4a24684256ec541d55
Pratham <mishrapratham.codeit@gmail.com>
2025-02-01 12:55:23 +0530

    d

--- main.cpp
+++ main.cpp
@@ -5,27 +5,19 @@ using namespace std;
 
 int32_t main() {
   PRATHAM
-  ll n, k; cin >> n >> k;
-  double low = 0;
-  double high = 0;
-  vector<double> a(n);
-  for (ll i = 0; i < n; i++) {
-    cin >> a[i];
-    high = max(high, a[i]);
+  ll n, x, y; cin >> n >> x >> y;
+  if (n == 1) 
+    cout << min(x, y) << endl;
+  else {
+    ll low = 0, high = n * min(x, y);
+    while (low <= high) {
+      ll mid = low + (high - low)/2;
+      if (((mid/x) + (mid/y)) < n-1)
+        low = mid+1;
+      else  
+        high = mid-1;
+    }
+    cout << low + min(x, y) << endl;
   }
-  auto f = [&](double mid) ->bool {
-    int count = 0;
-    for (auto& i : a) 
-      count += (int) (i/mid);
-    return count >= k;
-  };
-  while (high - low > 1e-6) {
-    double mid = low + (high-low)/2.0;
-    if (f(mid))
-      low = mid;
-    else
-      high = mid;
-  }
-  cout << fixed << setprecision(6) << low; 
   return 0;
 }
