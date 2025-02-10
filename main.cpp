5ab38767cd04359aa30216d042fc22c031df300d
Pratham <mishrapratham.codeit@gmail.com>
2025-02-10 17:05:08 +0530

    cleaned

--- main.cpp
+++ main.cpp
@@ -5,19 +5,5 @@ using namespace std;
 
 int32_t main() {
   PRATHAM
-  int n; cin >> n;
-  vector<int> a(n);
-  for (auto& i : a)
-    cin >> i;
-  int mx = a[0], res = a[0];
-  for (int i = 1; i < n; i++) {
-    mx = max(a[i], mx+a[i]);
-    res = max(res, mx);
-  }
-  cout << res << endl;
   return 0;
 }
-
-/*
--1 2 0 5 8 3 5 7
-*/
