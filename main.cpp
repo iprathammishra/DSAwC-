d5e216587f0b4a0024e7ccb248d76ae4b717b228
Pratham <mishrapratham.codeit@gmail.com>
2025-02-10 14:55:48 +0530

    d

--- main.cpp
+++ main.cpp
@@ -1,33 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define int long long
 #define PRATHAM ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
 int32_t main() {
   PRATHAM
-  ll tt; cin >> tt;
-  while (tt--) {
-    ll n, k; cin >> n >> k;
-    vector<ll> a(n);
-    for (auto& i : a)
-      cin >> i;
-    sort(begin(a), end(a));
-    ll L = 0, R = n-1;
-    ll res = 0;
-    while (L < R) {
-      ll sum = a[L]+a[R];
-      if (sum == k) 
-        {L++; R--; res++;}
-      else if (sum > k)
-        R--;
-      else
-        L++;
-    }
-    cout << res << endl;
+  int n; cin >> n;
+  set<pair<string, string>> S;
+  while (n--) {
+    pair<string, string> p;
+    cin >> p.first >> p.second;
+    S.insert(p);
   }
+  cout << (int)S.size() << endl;
   return 0;
 }
-
-/*
-3 1 4 1 5 9 2 6 5 3 5 8 9 7 9 3
-*/
